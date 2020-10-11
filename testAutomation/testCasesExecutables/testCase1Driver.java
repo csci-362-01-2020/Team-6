fe09d008bc81d2a39fe98973b006e8bc6c2b9278
Jellwood <jonellwood470@gmail.com>
2020-10-11 15:27:55 -0400

    Modified test driver

--- testAutomation/testCasesExecutables/testCase1Driver.java
+++ testAutomation/testCasesExecutables/testCase1Driver.java
@@ -1,17 +1,17 @@
-import User.java
-import Role.java
-import RoleConstants.java
+import org.openmrs.User;
 
-public static void main(String[] args) {
-    Role testRole = new Role("Doctor");
-    User testUser = new User();
-    testUser.addRole(testRole);
-    testUser.addRole(SUPERUSER);
-    boolean testOut = testUser.hasRole(args[0], args[1]);
-    if(testOut) {
-        System.out.println("true");
-    }
-    else {
-        System.out.println("false");
+public class testCase1Driver extends User {
+    public static void main(String[] args) {
+        Role testRole = new Role("Doctor");
+        User testUser = new User();
+        testUser.addRole(testRole);
+        testUser.addRole(SUPERUSER);
+        boolean testOut = testUser.hasRole(args[0], args[1]);
+        if(testOut) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
     }
 }
