0e293ddafd25cd2d062ab53b1a35d3b118640ecc
Christian Ellwood <38473927+Jellwood@users.noreply.github.com>
2020-10-27 08:50:41 -0400

    Add files via upload

---
+++ testAutomation/testCasesExecutables/testSystem.java
@@ -0,0 +1,38 @@
+public class testSystem {
+//aweful garbage code, written in microsoft office
+
+import java.util.Objects;
+import java.io.ObjectInputStream;
+
+private static ThreadSafeCircularFifoQueue ditty = new ThreadSafeCircularFifoQueue();
+
+boolean holdingValue;
+
+Object input_value;
+
+input_value = null;
+
+holdingValue = contains(input_value);
+System.out.printf("%B", holdingValue);
+
+input_value = 43
+
+holdingValue = contains(input_value);
+System.out.printf("%B", holdingValue);
+
+input_value = 2344
+
+holdingValue = contains(input_value);
+System.out.printf("%B", holdingValue);
+
+input_value = foo
+
+holdingValue = contains(input_value);
+System.out.printf("%B", holdingValue);
+
+input_value = barr
+
+holdingValue = contains(input_value);
+System.out.printf("%B", holdingValue);
+
+}
