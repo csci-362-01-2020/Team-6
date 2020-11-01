6d9a86d64646d5544be1f1dbcc568722ecd61adf
Jellwood <jonellwood470@gmail.com>
2020-11-01 12:43:07 -0500

    Functional script working no repetition or comparison

--- testAutomation/scripts/runAllTests.sh
+++ testAutomation/scripts/runAllTests.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+findDriver() {
+    cat $testDriver | while read line; do
+        if [[ $line =~ Test[[:space:]]Case[[:space:]]Driver ]]; then
+            CASEDRIVER=${line:17}       
+            echo $CASEDRIVER
+        fi
+    done
+}
+
+findValue() {
+    cat $testDriver | while read line; do
+        if [[ $line =~ Test[[:space:]]Data ]]; then 
+            CASEDATA=${line:10}
+            echo $CASEDATA
+        fi
+    done
+}
 
 list=$(find ../project -name 'ThreadSafeCircularFifoQueue.java')
 FILEPATH=${list[0]}
@@ -7,12 +24,20 @@ baseDirectory="../testCasesExecutables"
 
 cp $FILEPATH $baseDirectory
 
+testDriver=$(find ../testCases -name 'testCase1.txt')
+DRIVER=$(findDriver $testDriver)
+DRIVERNOEXT=${DRIVER:24:(-5)}
+
+DATA=$(findValue $testDriver)
+
 sed -i "s/package org.openmrs.util//" $baseDirectory/ThreadSafeCircularFifoQueue.java
 
-javac $baseDirectory/ThreadSafeCircularFifoQueue.java -d $baseDirectory && javac $baseDirectory/testCase1Driver.java -d $baseDirectory -cp $baseDirectory
-
+javac $baseDirectory/ThreadSafeCircularFifoQueue.java -d $baseDirectory && javac $DRIVER -d $baseDirectory -cp $baseDirectory
 cd $baseDirectory
-java testCase1Driver
+
+touch ../temp/out.html
+java $DRIVERNOEXT $DATA > ../temp/out.html
 
 rm $baseDirectory/ThreadSafeCircularFifoQueue.java
 rm $baseDirectory/*.class
+rm ../temp/out.html
--- testAutomation/testCasesExecutables/testCase1Driver.java
+++ testAutomation/testCasesExecutables/testCase1Driver.java
@@ -1,8 +1,6 @@
 public class testCase1Driver {
     public static void main(String[] args) {
         ThreadSafeCircularFifoQueue<Integer> testQueue = new ThreadSafeCircularFifoQueue<Integer>();
-        testQueue.add(17);
-        testQueue.add(21);
-        System.out.println(testQueue);
+        System.out.println(testQueue.contains(args[0]));
     }
 }
