0f48e467d71e48ea433921c784f5baf57549f4ff
Jellwood <jonellwood470@gmail.com>
2020-11-02 14:54:41 -0500

    Repetition works as intended, should work on merge, has been tested

--- testAutomation/scripts/runAllTests.sh
+++ testAutomation/scripts/runAllTests.sh
@@ -2,8 +2,8 @@
 findDriver() {
     cat $testDriver | while read line; do
         if [[ $line =~ Test[[:space:]]Case[[:space:]]Driver ]]; then
-            CASEDRIVER=${line:17}       
-            echo $CASEDRIVER
+            caseDriver=${line:17}       
+            echo $caseDriver
         fi
     done
 }
@@ -11,33 +11,49 @@ findDriver() {
 findValue() {
     cat $testDriver | while read line; do
         if [[ $line =~ Test[[:space:]]Data ]]; then 
-            CASEDATA=${line:10}
-            echo $CASEDATA
+            caseData=${line:10}
+            echo $caseData
         fi
     done
 }
 
+
+numFiles=$(ls ../testCases | wc -l)
 list=$(find ../project -name 'ThreadSafeCircularFifoQueue.java')
-FILEPATH=${list[0]}
-
+filePath=${list[0]}
 baseDirectory="../testCasesExecutables"
-
-cp $FILEPATH $baseDirectory
-
-testDriver=$(find ../testCases -name 'testCase1.txt')
-DRIVER=$(findDriver $testDriver)
-DRIVERNOEXT=${DRIVER:24:(-5)}
-
-DATA=$(findValue $testDriver)
-
+cp $filePath $baseDirectory
 sed -i "s/package org.openmrs.util//" $baseDirectory/ThreadSafeCircularFifoQueue.java
-
-javac $baseDirectory/ThreadSafeCircularFifoQueue.java -d $baseDirectory && javac $DRIVER -d $baseDirectory -cp $baseDirectory
-cd $baseDirectory
-
 touch ../temp/out.html
-java $DRIVERNOEXT $DATA > ../temp/out.html
 
-rm $baseDirectory/ThreadSafeCircularFifoQueue.java
-rm $baseDirectory/*.class
-rm ../temp/out.html
+for (( i = 1 ; i <= $numFiles ; i++)); do
+
+    unset driver
+    unset driver
+    unset driverNoExt
+    unset data
+
+
+    #echo "testCase"$i".txt"
+    testDriver=$(find ../testCases -name 'testCase'$i'.txt')
+    driver=$(findDriver $testDriver)
+    driver=$(basename $driver)
+    driver=${driver%$'\r'}
+
+    echo $driver
+
+    driverNoExt=${driver%.*}
+
+    data=$(findValue $testDriver)
+
+    cd $baseDirectory
+    javac ThreadSafeCircularFifoQueue.java -d $baseDirectory && javac $driver -d $baseDirectory -cp $baseDirectory
+
+    echo "Test "$i":" >> ../temp/out.html
+    java $driverNoExt $data >> ../temp/out.html
+    rm *.class
+
+done
+
+rm ThreadSafeCircularFifoQueue.java
+#rm ../temp/out.html
